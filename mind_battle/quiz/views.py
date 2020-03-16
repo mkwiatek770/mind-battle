1926d4f0778f183e2cac571cee3eaecadd1fffa6
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-16 22:52:00 +0100

    Category viewset

--- mind_battle/quiz/views.py
+++ mind_battle/quiz/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from quiz.models import Category
+from quiz.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
