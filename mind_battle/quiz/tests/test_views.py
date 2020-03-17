ecb019554e2772435faff6f7d59b35d6d3437b91
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-17 11:55:23 +0100

    Test for endpoints

--- mind_battle/quiz/tests/test_views.py
+++ mind_battle/quiz/tests/test_views.py
@@ -1,29 +1,72 @@
 """"Test endpoints for API."""
-import unittest
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from django.test import TestCase
+from django.utils import timezone
+from quiz.models import Quiz, Category
+from quiz.serializers import QuizSerializer, Question
 
 
-class TestQuizUnauthenticated(unittest.TestCase):
+class TestQuizUnauthenticated(TestCase):
     """Test for quiz related endpoints for unathenticated user."""
 
     def setUp(self):
-        self.request = APIRequestFactory()
+        self.client = APIClient()
 
     def test_get_all_published_quizzes(self):
         """Get all quizzes that was already published"""
-        pass
+        quiz_published = Quiz.objects.create(
+            name="quiz 1", date_published=timezone.now())
+        quiz_not_published = Quiz.objects.create(name="quiz 2")
+
+        response = self.client.get("/api/v1/quizzes/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(QuizSerializer(quiz_published).data, response.data)
+        self.assertNotIn(QuizSerializer(
+            quiz_not_published).data, response.data)
 
     def test_get_quizes_of_category(self):
         """Receive all quizzes by given category."""
-        pass
+        category_1 = Category.objects.create(name='python')
+        category_2 = Category.objects.create(name='javascript')
+        quiz_1 = Quiz.objects.create(name='quiz 1', category=category_1)
+        quiz_2 = Quiz.objects.create(name='quiz 2', category=category_2)
+
+        response = self.client.get('/api/v1/quizzes?category=python/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(QuizSerializer(quiz_1).data, response.data)
+        self.assertNotIn(QuizSerializer(quiz_2).data, response.data)
 
     def test_get_quiz_detail(self):
         """Receive detail info about specific quiz."""
-        pass
+        quiz = Quiz.objects.create(name='name')
+        serialized_data = QuizSerializer(quiz).data
+
+        response = self.client.get(f'/api/v1/quizzes/{quiz.id}')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, serialized_data)
 
     def test_get_quiz_questions(self):
         """Receive list of questions for specific quiz."""
-        pass
+        quiz = Quiz.objects.create(name='name')
+        question_1 = Question.objects.create(
+            quiz=quiz,
+            question="What is your favourite color?",
+            explaination="Some explaination")
+        question_2 = Question.objects.create(
+            quiz=quiz,
+            question="What is your name?",
+            explaination="Some explaination")
+
+        serialized_data = QuestionSerializer(
+            Question.objects.filter(quiz=quiz), many=True).data
+
+        response = self.client.get(f'/api/v1/quizzes/{quiz.id}/questions/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, serialized_data)
 
     def test_answer_to_question(self):
         """Submit answer to specific quiz question."""
@@ -34,11 +77,11 @@ class TestQuizUnauthenticated(unittest.TestCase):
         pass
 
 
-class TestQuizAuthenticated(unittest.TestCase):
+class TestQuizAuthenticated(TestCase):
     """Test for quiz related endpoints for unathenticated user."""
 
     def setUp(self):
-        self.request = APIRequestFactory()
+        self.client = APIClient()
 
     def test_get_draft_quizzes_for_user(self):
         """Test getting all drafted quizzes for creator."""
