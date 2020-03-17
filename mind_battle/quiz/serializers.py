519139e47685b478f1c4a8f006cac3986b91736e
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-17 11:53:15 +0100

    Question and Quiz serializers

--- mind_battle/quiz/serializers.py
+++ mind_battle/quiz/serializers.py
@@ -11,3 +11,21 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name')
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Quiz
+        fields = ('id', 'name', 'category', 'creator',
+                  'date_created', 'date_published', 'date_modified')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    quiz = QuizSerializer()
+
+    class Meta:
+        model = Question
+        fields = ('quiz', 'question', 'explaination',
+                  'good_answers', 'bad_answers')
