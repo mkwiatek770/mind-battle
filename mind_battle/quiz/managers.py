bfe1028886b23c7259ff0d6b4a3fee2d9d96dd03
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-18 13:15:37 +0100

    Refactor manager

--- mind_battle/quiz/managers.py
+++ mind_battle/quiz/managers.py
@@ -8,9 +8,9 @@ class QuizQueryset(models.QuerySet):
         """Return queryset of published quizzes."""
         return self.filter(date_published__isnull=False).order_by('-date_published')
 
-    def draft(self):
+    def drafts(self, user_id):
         """Return unpublished quizzes."""
-        return self.filter(date_published__isnull=True).order_by('-date_modified')
+        return self.filter(date_published__isnull=True, creator__id=user_id).order_by('-date_modified')
 
     def questions(self, quiz_id):
         """Return all quiz questions."""
@@ -23,14 +23,14 @@ class QuizManager(models.Manager):
     def get_queryset(self):
         return QuizQueryset(model=self.model, using=self._db)
 
-    def get_published(self):
+    def published(self):
         """Return queryset of published quizzes."""
         return self.get_queryset().published()
 
-    def get_drafts(self):
-        """Return unpublished quizzes."""
-        return self.get_queryset().draft()
+    def drafts(self, user_id):
+        """Return unpublished quizzes for specific user."""
+        return self.get_queryset().drafts(user_id)
 
-    def get_questions(self, quiz_id):
+    def questions(self, quiz_id):
         """Return all quiz questions."""
         return self.get_queryset().questions(quiz_id)
