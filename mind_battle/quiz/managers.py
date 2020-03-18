34284153e008991b16aae4f33d1527b248998c05
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-18 13:16:09 +0100

    Static typing

--- mind_battle/quiz/managers.py
+++ mind_battle/quiz/managers.py
@@ -8,11 +8,11 @@ class QuizQueryset(models.QuerySet):
         """Return queryset of published quizzes."""
         return self.filter(date_published__isnull=False).order_by('-date_published')
 
-    def drafts(self, user_id):
+    def drafts(self, user_id: int):
         """Return unpublished quizzes."""
         return self.filter(date_published__isnull=True, creator__id=user_id).order_by('-date_modified')
 
-    def questions(self, quiz_id):
+    def questions(self, quiz_id: int):
         """Return all quiz questions."""
         return self.get(pk=quiz_id).questions.all()
 
@@ -27,10 +27,10 @@ class QuizManager(models.Manager):
         """Return queryset of published quizzes."""
         return self.get_queryset().published()
 
-    def drafts(self, user_id):
+    def drafts(self, user_id: int):
         """Return unpublished quizzes for specific user."""
         return self.get_queryset().drafts(user_id)
 
-    def questions(self, quiz_id):
+    def questions(self, quiz_id: int):
         """Return all quiz questions."""
         return self.get_queryset().questions(quiz_id)
