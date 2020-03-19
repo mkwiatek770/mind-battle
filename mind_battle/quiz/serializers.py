2f232a1b48c0886ea09d49257aa9da26f46e697a
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-19 13:14:00 +0100

    Check if user is authenticated before creating/updating quiz

--- mind_battle/quiz/serializers.py
+++ mind_battle/quiz/serializers.py
@@ -16,13 +16,19 @@ class CategorySerializer(serializers.ModelSerializer):
 class QuizSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     category_name = serializers.CharField(write_only=True)
-    publish = serializers.BooleanField(write_only=True)
+    publish = serializers.BooleanField(write_only=True, required=False)
 
     def validate_category_name(self, value) -> str:
         """Check if category_name match with existing object in db."""
         if Category.objects.filter(name=value).exists():
             return value
         raise serializers.ValidationError("Category of given name does not exist")
+
+    def validate(self, data):
+        """Make sure that user is in request context."""
+        if self.context['request'].user.is_anonymous:
+            raise serializers.ValidationError("User is anonymous")
+        return data
 
     def create(self, validated_data) -> Quiz:
         """Create new quiz instance."""
@@ -31,12 +37,21 @@ class QuizSerializer(serializers.ModelSerializer):
             category=Category.objects.get(name=validated_data['category_name']),
             creator=self.context['request'].user
         )
-        if validated_data['publish']:
+        if validated_data.get('publish'):
             quiz.publish()
         return quiz
 
-    def update():
-        pass
+    def update(self, instance, validated_data):
+        """Update existing quiz instance."""
+        if validated_data.get("publish"):
+            instance.publish()
+        elif validated_data.get("publish") is False:
+            instance.unpublish()
+        if validated_data["category_name"] != instance.category.name:
+            category = Category.objects.get(name=validated_data["category_name"])
+            instance.category = category
+        instance.name = validated_data.get("name", instance.name)
+        instance.save()
 
     class Meta:
         model = Quiz
