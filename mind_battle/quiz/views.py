e71825f942378fdf86ed72091c5c4346c512f1e6
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-20 10:58:24 +0100

    Impelementation of removing quiz instance

--- mind_battle/quiz/views.py
+++ mind_battle/quiz/views.py
@@ -69,7 +69,9 @@ class QuizDetailView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        pass
+        quiz = self.get_object(pk)
+        quiz.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class QuizPublishView(APIView):
