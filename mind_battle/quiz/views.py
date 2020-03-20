538b17403f0621245f4a9c98ada08581e0150a28
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-20 12:53:48 +0100

    Logic to remove question object in view

--- mind_battle/quiz/views.py
+++ mind_battle/quiz/views.py
@@ -159,3 +159,8 @@ class QuestionDetailView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, quiz_pk, question_pk):
+        question = self.get_object(quiz_pk, question_pk)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
