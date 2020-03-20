8fcc2f4656b60d19fbbfe947be53382d9aa2e7a6
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-20 10:26:59 +0100

    Refactor code

--- mind_battle/quiz/permissions.py
+++ mind_battle/quiz/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 
 class IsCreator(BasePermission):
-    """Permission that check if request.user is creator"""
+    """Permission that check if request.user is object creator"""
 
     def has_object_permission(self, request, view, obj):
         return obj.creator == request.user
