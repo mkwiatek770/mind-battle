b9bec3d4b1ec26f983d858ae9e166e1e49f13597
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-21 16:16:36 +0100

    Add aliases to endpoints

--- mind_battle/quiz/urls.py
+++ mind_battle/quiz/urls.py
@@ -9,14 +9,15 @@ router = NestedDefaultRouter()
 #                         basename='question', parents_query_lookups=['quiz'])
 
 urlpatterns = [
-    path('quizzes/', views.QuizListView.as_view()),
-    path('quizzes/drafts/', views.QuizDraftsView.as_view()),
-    path('quizzes/<int:pk>/', views.QuizDetailView.as_view()),
-    path('quizzes/<int:pk>/questions/', views.QuestionsListView.as_view()),
-    path('quizzes/<int:pk>/publish/', views.QuizPublishView.as_view()),
-    path('quizzes/<int:pk>/unpublish/', views.QuizUnpublishView.as_view()),
-    path('quizzes/<int:pk>/image/', views.QuizImageView.as_view()),
-    path('quizzes/<int:quiz_pk>/questions/<int:question_pk>/', views.QuestionDetailView.as_view()),
+    path('quizzes/', views.QuizListView.as_view(), name='quiz_list'),
+    path('quizzes/drafts/', views.QuizDraftsView.as_view(), name='quiz_drafts'),
+    path('quizzes/<int:pk>/', views.QuizDetailView.as_view(), name='quiz_detail'),
+    path('quizzes/<int:pk>/questions/', views.QuestionsListView.as_view(), name='questtion_list'),
+    path('quizzes/<int:pk>/publish/', views.QuizPublishView.as_view(), name='quiz_publish'),
+    path('quizzes/<int:pk>/unpublish/', views.QuizUnpublishView.as_view(), name='quiz_unpublish'),
+    path('quizzes/<int:pk>/image/', views.QuizImageView.as_view(), name='quiz_image'),
+    path('quizzes/<int:quiz_pk>/questions/<int:question_pk>/',
+         views.QuestionDetailView.as_view(), name='question_detail'),
 
 ]
 
--- mind_battle/user/urls.py
+++ mind_battle/user/urls.py
@@ -4,10 +4,10 @@ from user import views
 
 
 urlpatterns = [
-    path('quizzes/<int:pk>/start/', views.QuizUserStartView.as_view()),
-    path('quizzes/<int:pk>/finish/', views.QuizUserFinishView.as_view()),
+    path('quizzes/<int:pk>/start/', views.QuizUserStartView.as_view(), name='quiz_start'),
+    path('quizzes/<int:pk>/finish/', views.QuizUserFinishView.as_view(), name='quiz_finish'),
     path('quizzes/<int:quiz_pk>/questions/<int:question_pk>/answer/',
-         views.QuestionUserAnswerView.as_view()),
+         views.QuestionUserAnswerView.as_view(), name='quiz_answer'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
