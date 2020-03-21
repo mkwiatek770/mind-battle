24be8be03b4c20abb1aacc5bf7a312a9a53e1162
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-21 16:17:12 +0100

    Remove unnnecesary code

--- mind_battle/quiz/urls.py
+++ mind_battle/quiz/urls.py
@@ -4,9 +4,6 @@ from quiz import views
 from quiz.router import NestedDefaultRouter
 
 router = NestedDefaultRouter()
-# quizzes_router = router.register(r'quizzes', views.QuizView, basename='quiz')
-# quizzes_router.register('questions', views.QuestionView,
-#                         basename='question', parents_query_lookups=['quiz'])
 
 urlpatterns = [
     path('quizzes/', views.QuizListView.as_view(), name='quiz_list'),
