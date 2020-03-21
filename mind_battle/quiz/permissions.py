e93527843e057570cef0ea3a26e260a5b2843c8b
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-21 14:24:32 +0100

    New permission to check is quiz object has been already published

--- mind_battle/quiz/permissions.py
+++ mind_battle/quiz/permissions.py
@@ -14,3 +14,10 @@ class IsQuizCreatorOrReadOnly(BasePermission):
 
         if request.method == 'GET' and quiz.date_published:
             return True
+
+
+class IsQuizPublished(BasePermission):
+    """Check if quiz object has been published."""
+
+    def has_object_permission(self, request, view, quiz):
+        return quiz.is_published
