b68c0cd57397e3d7ca46c94be4100fd7ce53b14b
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-23 09:17:26 +0100

    Register user model in administration panel

--- mind_battle/user/admin.py
+++ mind_battle/user/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from user.models import (
+    User,
     QuizUser,
     QuestionUser,
 )
 
-
+admin.site.register(User)
 admin.site.register(QuizUser)
 admin.site.register(QuestionUser)
