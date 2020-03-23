3406041f59c84786984193475aa823f1e72a40ff
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-23 14:20:39 +0100

    Test obtaining token on successful login

--- mind_battle/user/tests/test_authenticate.py
+++ mind_battle/user/tests/test_authenticate.py
@@ -1,7 +1,7 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
-from user.models import User
+from django.contrib.auth import get_user_model
 
 
 class TestRegisterUser(APITestCase):
@@ -20,3 +20,23 @@ class TestRegisterUser(APITestCase):
         response = self.client.post(reverse("register"), data=payload_data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class TestLoginUser(APITestCase):
+    """Test suite for logging user."""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='user',
+            password='password',
+            email='email@gmail.com'
+        )
+
+    def test_login_token_retrieve_successful(self):
+        """Assert token is obtained on successful credentials passed."""
+        payload_data = {'username': 'user', 'password': 'password'}
+
+        response = self.client.post(reverse('login'), data=payload_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data['token'])
