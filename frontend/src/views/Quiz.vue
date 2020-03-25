2f6e0cdbbb7bc732699d0dee4b212700b1226beb
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-25 12:17:31 +0100

    Modify views

---
+++ frontend/src/views/Quiz.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="quiz">
+    <QuizList msg="List of quizzes" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import QuizList from '@/components/QuizList.vue';
+
+export default {
+  name: 'Quiz',
+  components: {
+    QuizList,
+  },
+};
+</script>
