01ffdb76cef0127f12390e2a314972fcea52fda9
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-25 15:59:44 +0100

    Allow to redirect to start page view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav"><router-link to="/">Home</router-link> |</div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/start">Start Page</router-link>|
+    </div>
     <router-view />
   </div>
 </template>
