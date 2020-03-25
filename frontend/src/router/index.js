f6ecb9787c7d215914770c4835911b0d7bfbd903
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-25 15:26:30 +0100

    Views that will be needed

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,13 +11,24 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/register",
+    name: "UserRegister",
+    component: () => import("../views/UserRegister.vue")
+  },
+  {
+    path: "/quiz",
+    name: "QuizDetail",
+    component: () => import("../views/QuizDetail.vue")
+  },
+  {
+    path: "/quiz/new",
+    name: "QuizCreate",
+    component: () => import("../views/QuizCreate.vue")
+  },
+  {
+    path: "/quiz/edit",
+    name: "QuizEdit",
+    component: () => import("../views/QuizEdit.vue")
   }
 ];
 
