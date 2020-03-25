861ae48bc8fe920b1ebb1d299ddfc90c224a04d9
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-25 16:13:57 +0100

    Navbar component

---
+++ frontend/src/components/start-page/TheNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav id="start-nav">
+    <LoginForm />
+    <LoginButton />
+  </nav>
+</template>
+
+<script>
+import LoginButton from "./LoginButton.vue";
+import LoginForm from "./LoginForm.vue";
+
+export default {
+  name: "TheNavbar",
+  components: {
+    LoginForm,
+    LoginButton
+  },
+  data: () => {}
+};
+</script>
+
+<style scoped>
+#start-nav {
+  height: 50px;
+  background-color: rgb(69, 21, 172);
+  width: 100%;
+}
+</style>
