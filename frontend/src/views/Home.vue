65cf8f8fdb327df1ac358d3eb70686b9a6631103
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-25 15:06:57 +0100

    Another attempt to create vue project

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png"
-    >
-    <Login />
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Login from '@/components/Login.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Login,
-  },
+    HelloWorld
+  }
 };
 </script>
