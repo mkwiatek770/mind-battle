c2f5dab9a934cc4fa0a832f69f39455d846bac6f
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-26 14:18:18 +0100

    Add component description

--- frontend/src/components/home/SearchQuiz.vue
+++ frontend/src/components/home/SearchQuiz.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="search-quiz">
+    <p style="border-bottom: 1px solid black">SEARCH QUIZ</p>
     <form class="form-inline">
       <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search quiz" />
+        <input type="text" class="form-control" placeholder="Search" />
         <div class="input-group-append">
           <button class="btn btn-secondary" type="button">
             <i class="fa fa-search"></i>
