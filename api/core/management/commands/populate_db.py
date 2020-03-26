e027f1ef397d6b1abf8e70eecba76202a4f827de
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-26 09:27:30 +0100

    Faster implementation thanks to bulk_create

--- api/core/management/commands/populate_db.py
+++ api/core/management/commands/populate_db.py
@@ -5,13 +5,13 @@ from faker import Faker
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
+from django.utils import timezone
 from quiz.models import Quiz, Category, Question, QuestionAnswer
 
 
 class Command(BaseCommand):
     """Command to populate DB with many records."""
 
-    # przyjmować jako argument od użytkownika jedną z flag 0, 1, 2 gdzie 0 znaczy mało 1 średnio a 2 dużo
     # zamienić tworzenie na bulk_create
     # zamienić listę na set, żeby nie było problemu z unique constraint
 
@@ -56,7 +56,7 @@ class Command(BaseCommand):
         self.stdout.write(f"Time took: {t1-t0:.2f}s")
 
     def create_categories(self, n: int) -> List[Category]:
-        return [Category.objects.create(name=self.faker.word()) for _ in range(n)]
+        return [Category.objects.create(name=f"{self.faker.word()}{random.randint(1, 1000)}") for i in range(n)]
 
     def create_users(self, n: int) -> List[settings.AUTH_USER_MODEL]:
         users = []
@@ -72,32 +72,40 @@ class Command(BaseCommand):
         return users
 
     def create_quizzes(self, n: int, categories: list, users: list) -> None:
+        quizzes = []
         for _ in range(n):
             category = random.choice(categories)
             user = random.choice(users)
             quiz = Quiz.objects.create(
                 name=self.faker.sentence(),
                 creator=user,
-                category=category
+                category=category,
+                date_published=timezone.now() if random.random() > 0.5 else None
             )
-            if random.random() > 0.5:
-                quiz.publish()
 
             self.create_questions_to_quiz(quiz)
 
     def create_questions_to_quiz(self, quiz: Quiz) -> None:
-        for _ in range(random.randint(4, 8)):
-            question = Question.objects.create(
+        how_many = random.randint(4, 8)
+        question_objs = []
+        questions = {f"{self.faker.sentence()}{i}?" for i in range(how_many)}
+        for question in questions:
+            question_objs.append(Question(
                 quiz=quiz,
-                question=f"{self.faker.sentence()}?",
+                question=question,
                 explaination=self.faker.sentence()
-            )
+            ))
+        Question.objects.bulk_create(question_objs)
+
+        for question in Question.objects.filter(question__in=questions):
             self.create_answers_to_question(question)
 
     def create_answers_to_question(self, question: Question) -> None:
+        answers = []
         for i in range(random.randint(2, 4)):
-            QuestionAnswer.objects.create(
+            answers.append(QuestionAnswer(
                 question=question,
                 content=self.faker.word(),
                 is_correct=True if i == 0 else False
-            )
+            ))
+        QuestionAnswer.objects.bulk_create(answers, ignore_conflicts=True)
