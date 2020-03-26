49615f9c917035884a36deb83d4a6495eacaf58f
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-26 12:37:01 +0100

    Include all components in home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <TheNavbar />
+    <TheMain />
+    <TheSidebar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import TheNavbar from "@/components/home/TheNavbar.vue";
+import TheMain from "@/components/home/TheMain.vue";
+import TheSidebar from "@/components/home/TheSidebar.vue";
 
 export default {
   name: "Home",
-  components: {}
+  components: {
+    TheNavbar,
+    TheMain,
+    TheSidebar
+  }
 };
 </script>
