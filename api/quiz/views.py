c88bace198d50c55357f86a7fc5c9681d6877bb6
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-26 10:45:52 +0100

    data is empty so dont return empty dict

--- api/quiz/views.py
+++ api/quiz/views.py
@@ -204,7 +204,7 @@ class QuizImageView(APIView):
         serializer = ImageSerializer(quiz, data=request.data, context={'quiz_pk': pk})
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
