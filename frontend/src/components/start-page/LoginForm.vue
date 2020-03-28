a303f2baaa8eda5d2a42470f07cab8021c4e0512
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-28 17:14:08 +0100

    Bind input fields with data attributes

--- frontend/src/components/start-page/LoginForm.vue
+++ frontend/src/components/start-page/LoginForm.vue
@@ -1,23 +1,36 @@
 <template>
   <form class="form-inline align-middle" id="login-form">
     <div class="form-group mx-sm-3 mb-2">
-      <label for="inputPassword2" class="sr-only">Username</label>
+      <label for="input-username" class="sr-only">Username</label>
       <input
         type="text"
         class="form-control"
-        id="inputUsername"
+        id="input-username"
         placeholder="Username"
+        v-model="username"
       />
     </div>
     <div class="form-group mx-sm-3 mb-2">
-      <label for="inputPassword2" class="sr-only">Password</label>
+      <label for="input-password" class="sr-only">Password</label>
       <input
         type="password"
         class="form-control"
-        id="inputPassword2"
+        id="input-password"
         placeholder="Password"
+        v-model="password"
       />
     </div>
-    <button type="submit" class="btn btn-success mb-2">Login</button>
+    <button type="button" class="btn btn-success mb-2">Login</button>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  }
+};
+</script>
