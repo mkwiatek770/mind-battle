5b1bc60df000d7898c1708808a9da0e139d5f5cf
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-29 16:51:34 +0200

    Display alert

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,33 @@
 <template>
   <div id="app">
+    <nav v-if="alert.message">{{ alert.message }}</nav>
     <router-view />
   </div>
 </template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "app",
+  computed: {
+    ...mapState({
+      alert: state => state.alert
+    })
+  },
+  methods: {
+    ...mapActions({
+      clearAlert: "alert/clear"
+    })
+  },
+  watch: {
+    $route() {
+      // clear alert on location change
+      this.clearAlert();
+    }
+  }
+};
+</script>
 
 <style>
 #app {
