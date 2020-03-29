566b2dfe31899c673b0a2372d7a13fedb3fde1bf
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-29 14:14:09 +0200

    django-rest-swagger is deprecated

--- api/mind_battle/settings.py
+++ api/mind_battle/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
-    'rest_framework_swagger',
     # django built-in apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,7 +122,7 @@ REST_FRAMEWORK = {
     },
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication'
-    ]
+    ],
 
 }
 
