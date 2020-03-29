5d6afe441c5bb6c640662ef5d3bc02365788b98d
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-29 14:30:38 +0200

    Initial swagger api documentation

--- api/mind_battle/settings.py
+++ api/mind_battle/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     # 3rd party apps
     'storages',
+    'drf_yasg',
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
--- api/mind_battle/urls.py
+++ api/mind_battle/urls.py
@@ -1,13 +1,34 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from quiz.urls import router
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Mind Battle API",
+        default_version='v1',
+        description="Description in progress ...",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api/v1/', include('quiz.urls')),
     path('api/v1/', include('user.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+                                               cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+                                             cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
