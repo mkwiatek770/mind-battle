9ae2a68cd1aa9ef28410cf31306f0c62482efd13
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-29 15:51:07 +0200

    User service to store api functions

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,35 +1,78 @@
 import api from "@/services/api";
 
-export default {
-  login(username, password) {
-    return api
-      .post(`/auth/login/`, {
-        body: JSON.stringify({ username, password }),
-        headers: { "Content-Type": "application/json" }
-      })
-      .then(response => response.data)
-      .catch(error => {
-        console.log(error);
-      });
-  },
-  createAccount(data) {
-    return api
-      .post(`/auth/create-account`, {
-        body: JSON.stringify(data)
-      })
-      .then(response => response.data)
-      .catch(error => {
-        console.log(error);
-      });
-  },
-  refreshToken(data) {
-    return api
-      .post(`/auth/token/refresh`, {
-        body: JSON.stringify(data)
-      })
-      .then(response => response.data)
-      .catch(error => {
-        console.log(error);
-      });
-  }
+export const userService = {
+  login,
+  logout,
+  register,
+  refreshToken
 };
+
+const API_BASE_URL = "http://localhost:8000/api/v1";
+
+function login(username, password) {
+  const requestOptions = {
+    headers: { "Content-Type": "application/json" },
+    data: { username, password }
+  };
+
+  return api
+    .post(`${API_BASE_URL}/auth/login/`, requestOptions)
+    .then(handleResponse)
+    .then(user => {
+      // login successful if there's a jwt token in the response
+      if (user.token) {
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem("user", JSON.stringify(user));
+      }
+
+      return user;
+    });
+}
+
+function logout() {
+  // remove user from localStorage
+  localStorage.removeItem("user");
+}
+
+function register(user) {
+  const requestOptions = {
+    headers: { "Content-Type": "application/json" },
+    data: user
+  };
+
+  return api.post(`/auth/create-account/`, requestOptions).then(handleResponse);
+}
+
+function refreshToken(refresh) {
+  const requestOptions = {
+    headers: { "Content-Type": "application/json" },
+    data: { refresh: refresh }
+  };
+
+  return api
+    .post(`/auth/token/refresh/`, requestOptions)
+    .then(handleResponse)
+    .then(token => {
+      if (token.access) {
+        localStorage.setItem("accessToken", JSON.stringify(token.access));
+      }
+    });
+}
+
+function handleResponse(response) {
+  return response.text().then(text => {
+    const data = text && JSON.parse(text);
+    if (!response.ok) {
+      if (response.status === 401) {
+        // auto logout if 401 response returned from api
+        logout();
+        location.reload(true);
+      }
+
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
