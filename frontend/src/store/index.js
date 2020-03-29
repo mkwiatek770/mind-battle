0d64a5d1dceb7803c4fc514d2be96cecf92fb0e0
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-29 16:45:59 +0200

    alert store module

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import alert from "./modules/alert";
 import quiz from "./modules/quiz";
 import user from "./modules/user";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { quiz, user }
+  modules: { alert, quiz, user }
 });
