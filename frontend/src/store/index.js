b612d845cec2dcfa5b51e004465d789c14b211b5
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-30 09:08:37 +0200

    persist state data on page refresh

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 import alert from "./modules/alert";
 import quiz from "./modules/quiz";
 import user from "./modules/user";
@@ -7,5 +8,6 @@ import user from "./modules/user";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { alert, quiz, user }
+  modules: { alert, quiz, user },
+  plugins: [createPersistedState()]
 });
