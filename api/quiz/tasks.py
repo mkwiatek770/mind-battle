7c1ebd1cf12df6b9bd5d6055643d775e10a2e333
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-31 08:52:01 +0200

    Resize quiz images that are stored on S3

--- api/quiz/tasks.py
+++ api/quiz/tasks.py
@@ -12,31 +12,20 @@ def resize_quiz_image(quiz_id: int) -> None:
     resize_quiz_photo_command(Quiz.objects.get(id=quiz_id))
 
 
-def resize_quiz_photo_command(quiz: Quiz):
-    _, extension = os.path.splitext((quiz.image.url.split("?")[0]))
-    photo = default_storage.open(quiz.image.url)
-    resized_photo = Image.open(photo.file).resize((300, 200), Image.ANTIALIAS)
-    b = io.BytesIO()
-    resized_photo.save(b, format=extension[1:])
-    resp = default_storage.save(quiz.image.url, ContentFile(b.getvalue()))
-#    quiz.cover_photo = os.path.join('books', os.path.basename(resp))
-    quiz.save()
+def resize_quiz_photo_command(quiz: int) -> None:
+    """Resize image of single quiz object."""
+    if quiz.image.name == 'default_quiz.jpeg':
+        return
 
-
-def resize_quiz_photo_command_v2(quiz: int) -> None:
     memfile = io.BytesIO()
-
     img = Image.open(quiz.image)
 
     output_size = (400, 300)
     img.thumbnail(output_size, Image.ANTIALIAS)
     img.save(memfile, 'JPEG', quality=95)
-    # default_storage.save(quiz.image.name, memfile)
-    memfile.close()
-    img.close()
 
-    # quiz.image.save(img)
-    # import pdb
-    # pdb.set_trace()
-    # quiz.image = img
-    # quiz.save()
+    old_image = quiz.image
+    old_image_name = old_image.name
+
+    default_storage.delete(old_image.name)
+    quiz.image.save(old_image_name, ContentFile(memfile.getvalue()))
