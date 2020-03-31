bcb1ced5bc09c2ac918510a0fc7b4ba3a8bc7a4b
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-03-31 11:08:49 +0200

    Activate signal

--- api/quiz/apps.py
+++ api/quiz/apps.py
@@ -5,3 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 class QuizConfig(AppConfig):
     name = 'quiz'
     verbose_name = _('quizzes')
+
+    def ready(self):
+        import quiz.signals
