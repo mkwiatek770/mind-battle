bba35f3c1dc6894782ffa74f18ed759362047ec8
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-02 09:50:55 +0200

    Quiz summary view

---
+++ frontend/src/components/quiz-summary/SummaryMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Summary</h1>
+    <h5>Your score: 72%</h5>
+    <p>You completed "Quiz name" with 8 correct answers out of 11 questions</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState("user", ["quiz"])
+  }
+};
+</script>
---
+++ frontend/src/views/QuizSummary.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="quiz-summary">
+    <TheNavbar />
+    <SummaryMain />
+  </div>
+</template>
+
+<script>
+import TheNavbar from "@/components/base/TheNavbar.vue";
+import SummaryMain from "@/components/quiz-summary/SummaryMain.vue";
+
+export default {
+  name: "QuizSummary.vue",
+  components: {
+    TheNavbar,
+    SummaryMain
+  }
+};
+</script>
