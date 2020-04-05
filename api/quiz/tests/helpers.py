1e7b3f79881c2fb3de0f35a1dfd2d2e74ca18a54
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-05 11:29:04 +0200

    Fix helper function

--- api/quiz/tests/helpers.py
+++ api/quiz/tests/helpers.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from quiz.models import Quiz, Question, QuestionAnswer
 
 
-def create_user(username: str, email: str, password: str) -> settings.AUTH_USER_MODEL:
-    return get_user_model.objects.create_user(
+def create_user(username: str, password: str, email: str) -> settings.AUTH_USER_MODEL:
+    return get_user_model().objects.create_user(
         username=username,
         email=email,
         password=password
