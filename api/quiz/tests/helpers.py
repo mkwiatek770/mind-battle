2c339691c0a1f090d55cdce698f7194ed74de856
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-05 11:57:13 +0200

    Docstrings

--- api/quiz/tests/helpers.py
+++ api/quiz/tests/helpers.py
@@ -4,6 +4,11 @@ from quiz.models import Quiz, Question, QuestionAnswer, Category
 
 
 def create_user(username: str, password: str, email: str) -> settings.AUTH_USER_MODEL:
+    """
+    Create AUTH_USER_MODEL instance.
+
+    To make function quicker, password is not hashed.
+    """
     return get_user_model().objects.create(
         username=username,
         email=email,
@@ -12,16 +17,20 @@ def create_user(username: str, password: str, email: str) -> settings.AUTH_USER_
 
 
 def create_quiz(**kwargs) -> Quiz:
+    """Create Quiz instance."""
     return Quiz.objects.create(**kwargs)
 
 
 def create_question(**kwargs) -> Question:
+    """Create Question instance."""
     return Question.objects.create(**kwargs)
 
 
 def create_category(**kwargs) -> Category:
+    """Create Category instance."""
     return Category.objects.create(**kwargs)
 
 
 def create_question_answer(**kwargs) -> QuestionAnswer:
+    """Create QuestionAnswer instance."""
     return QuestionAnswer.objects.create(**kwargs)
