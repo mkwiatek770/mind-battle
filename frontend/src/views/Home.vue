c426fa94f20b0fa0e062d8a91a3edb47d8f2ef23
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-06 14:20:01 +0200

    Change location of pagination component in home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,13 +4,11 @@
     <div class="row">
       <div class="col-sm-12 col-md-8 col-lg-9">
         <TheMain />
+        <ThePagination />
       </div>
       <div class="col-sm-12 col-md-4 col-lg-3">
         <TheSidebar />
       </div>
-    </div>
-    <div class="row">
-      <ThePagination />
     </div>
   </div>
 </template>
