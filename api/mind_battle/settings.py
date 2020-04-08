db0c9be3b00af4f7c09af09d8241f11b76178fe5
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-08 14:02:18 +0200

    Replace postgresql_psycopg2 engine with postgresql engine. It's still the same one but this is supported from newer django version

--- api/mind_battle/settings.py
+++ api/mind_battle/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'mind_battle.wsgi.application'
 if env('ENVIRONMENT') == 'production':
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get("DB_NAME"),
             'USER': os.environ.get("DB_USER"),
             'PASSWORD': os.environ.get("DB_PASSWORD"),
