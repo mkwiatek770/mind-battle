e37b5f68cbdb57dd3b5990f6cfe3771d89cb50a6
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-08 16:48:23 +0200

    Axios interceptor in api.js file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,28 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export default axios.create({
+const api = axios.create({
   baseURL: "http://localhost:8000/api/v1",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
-    "X-CSRFToken": Cookies.get("csrftoken")
-  }
+    "X-CSRFToken": Cookies.get("csrftoken"),
+  },
 });
+
+api.interceptors.response.use(
+  function(response) {
+    console.log("Git");
+    console.log(response);
+    return response;
+  },
+  function(error) {
+    console.log("Err");
+    console.log(error);
+  }
+);
+
+export default api;
 
 // export const authenticationHeader = () => {
 //   return {
