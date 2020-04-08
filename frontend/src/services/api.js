65c1d4be329f8172d04c75d4261336bd44cd8b24
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-08 17:12:06 +0200

    Logout user if refresh token is invalid

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 import store from "@/store";
+import router from "../router";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api/v1",
@@ -16,19 +17,12 @@ api.interceptors.response.use(
     return response;
   },
   function(error) {
-    // Return any error which is not due to authentication back to the calling service
-    // if (error.response.status !== 401){
-    //   return new Promise((resolve, reject) => {
-    //     reject(error);
-    //   })
-    // }
-
-    // // Logout user if token refresh didn't work or user is disabled
-    // if (error.config.url == '/api/token/refresh' || error.response.message == 'Account is disabled.') {
-    //   router PushManager()
-    // }
-
     if (error.response.status === 401) {
+      // Logout user if token refresh didn't work
+      if (error.config.url == "/auth/token/refresh") {
+        router.push("/");
+      }
+
       // Try request again with new token
       store.dispatch("refreshToken", store.refreshToken).then((token) => {
         // new request with new token
