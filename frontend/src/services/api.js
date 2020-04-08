c4c52f81e039359605b52ab141e766624dd6673f
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-08 17:09:00 +0200

    Initial version of interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+import store from "@/store";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api/v1",
@@ -12,13 +13,44 @@ const api = axios.create({
 
 api.interceptors.response.use(
   function(response) {
-    console.log("Git");
-    console.log(response);
     return response;
   },
   function(error) {
-    console.log("Err");
-    console.log(error);
+    // Return any error which is not due to authentication back to the calling service
+    // if (error.response.status !== 401){
+    //   return new Promise((resolve, reject) => {
+    //     reject(error);
+    //   })
+    // }
+
+    // // Logout user if token refresh didn't work or user is disabled
+    // if (error.config.url == '/api/token/refresh' || error.response.message == 'Account is disabled.') {
+    //   router PushManager()
+    // }
+
+    if (error.response.status === 401) {
+      // Try request again with new token
+      store.dispatch("refreshToken", store.refreshToken).then((token) => {
+        // new request with new token
+        const config = error.config;
+        config.headers["Authorization"] = `Bearer ${token}`;
+
+        return new Promise((resolve, reject) => {
+          api
+            .request(config)
+            .then((response) => {
+              resolve(response);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        });
+      });
+    }
+
+    return new Promise((resolve, reject) => {
+      reject(error);
+    });
   }
 );
 
