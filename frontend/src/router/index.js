513fe2bf07cbdbe7154fa13a84efaa760c2b44ab
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-08 14:41:46 +0200

    Clean in router file

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,6 @@
-// import axios from "axios";
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-// import { Store } from "vuex";
-// import store from "store";
 
 Vue.use(VueRouter);
 
@@ -13,110 +10,78 @@ const routes = [
     name: "StartPage",
     component: () => import("../views/StartPage.vue"),
     meta: {
-      guest: true
-    }
+      guest: true,
+    },
   },
   {
     path: "/",
     name: "Home",
     component: Home,
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
     path: "/quiz/new",
     name: "QuizCreate",
     component: () => import("../views/QuizCreate.vue"),
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
     path: "/quiz/edit",
     name: "QuizEdit",
     component: () => import("../views/QuizEdit.vue"),
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
     path: "/quiz/solve",
     name: "QuizSolve",
     component: () => import("../views/QuizSolve.vue"),
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
     path: "/quiz/summary",
     name: "QuizSummary",
     component: () => import("../views/QuizSummary.vue"),
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
     path: "/quiz/:id",
     name: "QuizDetail",
     component: () => import("../views/QuizDetail.vue"),
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
 
   // otherwise redirect to home
-  { path: "*", redirect: "/" }
+  { path: "*", redirect: "/" },
 ];
-
-// let isRefreshing = false;
-
-// axios.interceptors.response.use(
-//   response => {
-//     return response;
-//   },
-//   error => {
-//     const {
-//       config,
-//       response: { status, data }
-//     } = error;
-
-//     const originalRequest = config;
-
-//     if (status === 401 && data.message == "Token is invalid or expired") {
-//       if (!isRefreshing) {
-//         isRefreshing = true;
-//         store
-//           .dispatch("refreshToken")
-//           .then(({ status }) => {
-//             if (status === 200 || status === 204) {
-//               isRefreshing = false;
-//             }
-//           })
-//           .catch(error => {
-//             console.error(error);
-//           });
-//       }
-//     }
-//   }
-// );
 
 const router = new VueRouter({
   routes,
-  mode: "history"
+  mode: "history",
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
     if (!JSON.parse(localStorage.vuex).user.accessToken) {
       next({
         path: "/start",
-        params: { nextUrl: to.fullPath }
+        params: { nextUrl: to.fullPath },
       });
     } else {
       next();
     }
-  } else if (to.matched.some(record => record.meta.guest)) {
+  } else if (to.matched.some((record) => record.meta.guest)) {
     next();
   }
   next();
